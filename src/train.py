486a82663c292aa2541255932652201c7e1310d3
venkat-2608 <sudheendra.mv@gmail.com>
2026-03-21 19:02:48 +0530

    added initial logistic regression training script

---
+++ src/train.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv("data/raw/churn.csv")
+df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors="coerce")
+df = df.dropna()
+
+for col in df.select_dtypes(include="object").columns:
+    df[col] = LabelEncoder().fit_transform(df[col])
+X = df.drop("Churn", axis=1)
+y = df["Churn"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+preds = model.predict(X_test)
+acc = accuracy_score(y_test, preds)
+
+print("Model Accuracy:", acc)
